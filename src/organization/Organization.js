2d9ea64e168df1caba423d5c88d13038da23e874
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-12-08 20:53:52 -0300

    :star2: add Organization class

---
+++ src/organization/Organization.js
@@ -0,0 +1,12 @@
+class Organization {
+    name;
+    cnpj;
+  
+    constructor(name, cnpj) {
+      this.name = name;
+      this.cnpj = cnpj;
+    }
+  }
+  
+  module.exports = Organization;
+  
