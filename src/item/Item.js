b94d32c22fb0dbb36017fb462b7b5136241bfca2
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-12-08 20:55:22 -0300

    :star2: add Item class

---
+++ src/item/Item.js
@@ -0,0 +1,9 @@
+class Item {
+  constructor(nameItem, description, amount) {
+    this.nameItem = nameItem;
+    this.description = description;
+    this.amount = amount;
+  }
+}
+
+module.exports = Item;
